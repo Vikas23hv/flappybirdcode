1ffccd8cda23dd416526f6aa46dda1347839da38
Vikas23hv <118504837+Vikas23hv@users.noreply.github.com>
2023-09-17 11:50:39 +0530

    Changing Spawn Rate

--- Pipespawnerc.cs
+++ Pipespawnerc.cs
@@ -7,7 +7,7 @@ public class Pipespawnerc : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject pipe;
-    public float spawnRate = 2;
+    public float spawnRate = 2.5;
     private float timer;
     public float heightOffset = 10;
     void Start()
